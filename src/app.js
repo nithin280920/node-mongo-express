fb2aefbfa72e76f257720962a08f884d0d4289a0
nithin280920 <nithin280920@gmail.com>
2025-12-02 17:06:24 +0530

    # route setup with user

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 require('dotenv').config(); 
 const express = require('express');
 const cors = require('cors');
+const routes = require('./routers/index'); // Assuming you have a routes.js file in the same directory
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/v1", routes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
